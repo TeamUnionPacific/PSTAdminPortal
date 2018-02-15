05c7d257001eed524a192721b4b63c8de298585b
cse498 <mcgeeaus@msu.edu>
2018-02-15 16:06:00 -0500

    Updating github project

---
+++ js/login.js
@@ -0,0 +1,39 @@
+// console.log(document.getElementById("login"));
+//
+//
+// document.getElementById("login").onclick = function () {
+//     Submit();
+// }
+
+function Submit(user, pw) {
+
+
+    if(username == "" || pw == ""){
+        return false
+    }
+
+    /*
+    access java webservice here
+    should return a true/false from the java file
+    true > correct user/pw - set session to user and return true
+    false > incorrect user/pw - show error of incorrect user/pw
+    */
+    return true;
+}
+
+
+window.onload = function (ev) {
+    console.log("loaded");
+    document.getElementById("submit").onclick = function () {
+        var username = document.getElementById('login-input');
+        var pw = document.getElementById('pw-input');
+        if(username == ""){
+
+        }
+        if(Submit()){
+            window.location.href = 'landing.html'
+        }else{
+
+        }
+    }
+}
